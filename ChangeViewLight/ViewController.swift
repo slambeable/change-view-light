133e7b27e487a70ace7b02624ffff7fb9f0cfb21
slambeable <41022916+slambeable@users.noreply.github.com>
2021-12-17 15:54:40 +0300

    create progect

--- ChangeViewLight/ViewController.swift
+++ ChangeViewLight/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewColor: UIView!
+
+    @IBOutlet var redSliderValueLabel: UILabel!
+    @IBOutlet var greenSliderValueLabel: UILabel!
+    @IBOutlet var blueSliderValueLabel: UILabel!
+    
+    private var red = CGFloat(1)
+    private var green = CGFloat(1)
+    private var blue = CGFloat(1)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColor.layer.cornerRadius = 16
     }
+    
+    @IBAction func changeSliderValue(_ sender: UISlider) {
+        if let restorationIdentifier = sender.restorationIdentifier {
+            let colorOfView = ColorsOfView(rawValue: restorationIdentifier)
+            
+            switch colorOfView {
+            case .red:
+                red = CGFloat(sender.value)
+                redSliderValueLabel.text = String(format: "%.2f", sender.value)
+            case .green:
+                green = CGFloat(sender.value)
+                greenSliderValueLabel.text = String(format: "%.2f", sender.value)
+            case .blue:
+                blue = CGFloat(sender.value)
+                blueSliderValueLabel.text = String(format: "%.2f", sender.value)
+            case .none:
+                print("error")
+            }
+        }
 
-
+        viewColor.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 }
 
+extension ViewController {
+    private enum ColorsOfView: String {
+    case red
+    case green
+    case blue
+    }
+}
